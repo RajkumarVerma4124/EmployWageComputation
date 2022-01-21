734c8ee39df060153ce7b12bdb5df208d1272247
[RajkumarVerma4124] <[vermar4124@gmail.com]>
2022-01-21 14:12:26 +0530

    [Rajkumar] Added:- The code to check employ attendence

--- EmployeeWageComputation/Program.cs
+++ EmployeeWageComputation/Program.cs
@@ -8,11 +8,28 @@ namespace EmployeeWageComputation
 {
     internal class Program
     {
+        //Initializing Constant
+        private const int FULL_TIME_EMP = 1;
+
         static void Main(string[] args)
         {
             //Entered the welcome message
             Console.WriteLine("Welcome To The Employee Wage Computation Program");
             Console.ReadLine();
+
+            //Check Employ Is Present Or Absent(UC1)
+            Random randCheck = new Random();
+            int checkAttend = randCheck.Next(1, 3);
+            if (checkAttend == FULL_TIME_EMP)
+            {
+                Console.WriteLine("Employee is present");
+                Console.ReadLine();
+            }
+            else
+            {
+                Console.WriteLine("Employee is absent");
+                Console.ReadLine();
+            }
         }
     }
 }
