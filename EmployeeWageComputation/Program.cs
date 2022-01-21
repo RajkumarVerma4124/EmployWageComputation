5ac2dc69b7b68e517fb7908a6aaef9750511ae24
[RajkumarVerma4124] <[vermar4124@gmail.com]>
2022-01-21 14:48:36 +0530

    [Rajkumar] Added:- The code to calc part time empwage

--- EmployeeWageComputation/Program.cs
+++ EmployeeWageComputation/Program.cs
@@ -10,8 +10,10 @@ namespace EmployeeWageComputation
     {
         //Initializing Constant
         private const int FULL_TIME_EMP = 1;
+        private const int PART_TIME_EMP = 2;
         private const int WAGE_PER_HOUR = 20;
         private const int FULL_DAY_HOUR = 8;
+        private const int HALF_DAY_HOUR = 4;
 
         //Initializing Variables
         private static int dailyEmpHrs = 0;
@@ -25,23 +27,31 @@ namespace EmployeeWageComputation
 
             //Check Employ Is Present Or Absent(UC1)          
             Random randCheck = new Random();
-            int checkAttend = randCheck.Next(1, 3);
+            int checkAttend = randCheck.Next(0, 3);
             if (checkAttend == FULL_TIME_EMP)
             {
-                Console.WriteLine("Employee is present");
+                Console.WriteLine("Employee is present for full time");
                 dailyEmpHrs = FULL_DAY_HOUR;
+                Console.ReadLine();
+            }
+            else if (checkAttend == PART_TIME_EMP)
+            {
+                Console.WriteLine("Employee is present for part time");
+                dailyEmpHrs = HALF_DAY_HOUR;
                 Console.ReadLine();
             }
             else
             {
-                Console.WriteLine("Employee is absent");
+                Console.WriteLine("Employee is absent for today");
                 dailyEmpHrs = 0;
                 Console.ReadLine();
             }
 
-            //Calculate Employ Daily Wage (UC2)
+            //Calculate Employ Daily Wage And Part Time Wage (UC2 & UC3)
             dailyEmpWage = WAGE_PER_HOUR * dailyEmpHrs;
-            Console.WriteLine("Employ Todays Wage is :"+dailyEmpWage);
+            Console.WriteLine("Employ Todays Wage is : " +
+                "" +
+                ""+dailyEmpWage);
             Console.ReadLine();
         }
     }
